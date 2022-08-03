97ed03422dacc6531e0f85af0aedc45e10a700c5
IG <iikim2511@gmail.com>
2022-08-03 02:21:18 -0400

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,14 @@
 import './App.css';
 import data from './data';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import HomeScreen from './screens/HomeScreen';
+
 /* 
   🦄🦄c6. 리액트폴더안의 Git 삭제하기, 상위폴더에 Git설치하기
 
@@ -14,44 +22,86 @@ import data from './data';
   👉 index.css
 
   frontend폴더안으로 들어간 후, npm start   
+
+
+  🦄🦄c8. Add Routing, router-dom, URL parameter, useParams, Link to=
+
+    router-dom
+    URL parameter
+    useParams
+    Link to=
+
+    1 click home screen
+
+    2. loading spin mark 확인됨
+
+
+    react는 1개의 페이지에서 작동하는것.
+
+    page refresh하면 안되는것임
+
+    Link 태그를 활용하자
+
+
+    Link is a component from React router dom
 */
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">kim's amazon</a>
-      </header>
-      <main>
-        <h1> Featured Products</h1>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">kim's amazon</a>
+        </header>
+        <main>
 
-        <div className="products">
-        
-          {/* 🍀c7. ~.map(~) */}
-          {
-            data.data_products.map((p_product)=>(
+        {/* 🍀c8 router-dom
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="teams" element={<Teams />}>
+                <Route path=":teamId" element={<Team />} />
+                <Route path="new" element={<NewTeamForm />} />
+                <Route index element={<LeagueStandings />} />
+              </Route>
+            </Route>
+          </Routes>
+        */}
 
-                // 🍀c7.  key={} : map()안의 첫번째 태그에 추가
-              <div className="product" key={p_product.slug}>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
 
-                    {/*  🍀c7.  href={ `~~~`}
-                       slug사용해서 address 세팅 */}
-                <a href={`/product/${p_product.slug}`}>.
-                  <img src={p_product.image} alt={p_product.name}/>
-                </a>
+          <h1> Featured Products</h1>
 
-                <div className="product-info">
-                  <a href={`/product/${p_product.slug}`}>
-                    <p>{p_product.name}</p>
+          <div className="products">
+          
+            {/* 🍀c7. ~.map(~) */}
+            {
+              data.data_products.map((p_product)=>(
+
+                  // 🍀c7.  key={} : map()안의 첫번째 태그에 추가
+                <div className="product" key={p_product.slug}>
+
+                      {/*  🍀c7.  href={ `~~~`}
+                        slug사용해서 address 세팅 */}
+                  <a href={`/product/${p_product.slug}`}>.
+                    <img src={p_product.image} alt={p_product.name}/>
                   </a>
-                    <p><strong>${p_product.price}</strong></p>    
-                </div>        
-              </div>
-            ))
-          }
-        </div>      
-      </main>    
-    </div>
+
+                  <div className="product-info">
+                    <a href={`/product/${p_product.slug}`}>
+                      <p>{p_product.name}</p>
+                    </a>
+                      <p><strong>${p_product.price}</strong></p>    
+                  </div>        
+                </div>
+              ))
+            }
+          </div>      
+        </main>    
+      </div>
+    </BrowserRouter>
   );
 }
 
